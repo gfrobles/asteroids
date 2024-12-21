5068202407880682b6afb7d455c5c4108e31d14e
gfrobles <gfrobles@gmail.com>
2024-12-21 06:31:58 -0800

    Added 60 fps to the game

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +19,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        time_passsed = clock.tick(60)
+        dt = time_passsed/1000
 
 
 if __name__ == "__main__":
